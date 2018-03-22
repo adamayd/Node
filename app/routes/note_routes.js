960d1ad82370cb353db836cbfe71abd9bb7d8d2b
Adam Ayd <adam.ayd@gmail.com>
2018-03-21 23:20:44 -0400

    finished the CRUD routes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,12 +1,47 @@
+const ObjectID = require('mongodb').ObjectID
+
 module.exports = function(app, db) {
   app.post('/notes', (req, res) => {
-    //We will create the note here
-    const note = { text: req.body.body, title: req.body.title }
+    const note = {text: req.body.body, title: req.body.title}
     db.collection('notes').insert(note, (err, result) => {
       if(err) {
-        res.send( { 'error': 'An error has occcured' });
+        res.send({'error': 'A create error has occcured'})
       } else {
         res.send(result.ops[0])
+      }
+    })
+  })
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id
+    const details = {'_id': new ObjectID(id)}
+    db.collection('notes').findOne(details, (err, item) => {
+      if(err) {
+        res.send({'error': 'A read error has occured'})
+      } else {
+        res.send(item)
+      }
+    })
+  })
+  app.put('/notes/:id', (req, res) => {
+    const id = req.params.id
+    const details = {'_id': new ObjectID(id)}
+    const note = {text: req.body.body, title: req.body.title}
+    db.collection('notes').update(details, note, (err, item) => {
+      if(err) {
+        res.send({'error': 'An update error has occured'})
+      } else {
+        res.send(item)
+      }
+    })
+  })
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id
+    const details = {'_id': new ObjectID(id)}
+    db.collection('notes').remove(details, (err, item) => {
+      if(err) {
+        res.send({'error': 'A delete error has occured'})
+      } else {
+        res.send(`Note ${id} deleted`)
       }
     })
   })
